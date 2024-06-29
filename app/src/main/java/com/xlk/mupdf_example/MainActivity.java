4bc4d1a1e23748fb7b9e10ea75ea00de88cc020c
咕噜咕噜 <xlk214501580@163.com>
2024-06-29 14:44:05 +0800

    app 模块使用

--- app/src/main/java/com/xlk/mupdf_example/MainActivity.java
+++ app/src/main/java/com/xlk/mupdf_example/MainActivity.java
@@ -19,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        copyAssets(this, "abcde.pdf");
+        File file = new File(getExternalCacheDir().getAbsolutePath() + File.separator + "abcde.pdf");
+        if(!file.exists()) {
+            copyAssets(this, "abcde.pdf");
+        }
     }
 
     private void copyAssets(Context context, String fileName) {
